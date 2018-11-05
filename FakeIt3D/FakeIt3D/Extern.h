a59491229bf62b9ecd63e0c0fe07d6b8b717ed88
Henrik Nilsson <henrik.n@live.se>
2018-11-05 21:16:14 +0100

    Changes to files

--- FakeIt3D/FakeIt3D/Extern.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <d3d11.h>
-#include <DirectXMath.h>
-namespace DX
-{
-	extern ID3D11Device* g_device; 
-	extern ID3D11DeviceContext* g_deviceContext; 
-	extern IDXGISwapChain* g_swapChain; 
-}
